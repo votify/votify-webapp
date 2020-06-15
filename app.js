ad7304197752014fe324b5f1a5fd4cbe3af742fc
ngankhanh98 <ngankhanh98@gmail.com>
2020-06-15 09:15:44 +0700

    reinit project

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
-const router = express.Router();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.use(morgan("dev"));
-app.use(cors());
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("view engine", "hbs");
-app.engine(
-  "hbs",
-  handlebars({
-    layoutsDir: __dirname + "/views/layouts",
-    extname: "hbs",
-  })
-);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.use("/hello", require("./routes/main.route"));
-
-app.use((req, res, next) => {
-  res.status(404).send("NOT FOUND");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
-  const code = err.code || 500;
-  console.log(code, err.message);
-  res.status(code).send(err.message);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, (_) => {
-  console.log(`Webapp is running at http://localhost:${PORT}`);
-});
+module.exports = app;

f2ada0a489453a0cb3209f297588db669e1c2b57
ngankhanh98 <ngankhanh98@gmail.com>
2020-06-16 11:28:50 +0700

    post

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require("express");
+const secp256k1 = require("secp256k1");
+const crypto = require("crypto");
+const axios = require("axios");
 var router = express.Router();
 
 /* GET home page. */
@@ -10,7 +13,40 @@ router.get("/register", (req, res) => {
   res.render("register", { layout: "layout" });
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
+  // const {type, data, signature, lock} // (hash) public + cmnd + ten +ngay thang nam sinh
+  // const { data } = req.body;
+  const data = { ...req.body };
+  const { idnumber, fullname, birthday, birthmonth, birthyear } = data;
+
+  const privKey = crypto
+    .createHash("sha256")
+    .update(JSON.stringify(data))
+    .digest("hex");
+
+  const pubKey = secp256k1.publicKeyCreate(privKey);
+
+  const dob = new Date(birthyear, birthmonth, birthday);
+  const _data = {
+    type: "users",
+    data: { id: idnumber, name: fullname, dob: dob, pubKey },
+    signature: null,
+    lock: null,
+  };
+  const url = "https://blockchain-node-01.herokuapp.com/";
+  await axios
+    .post(url, { _data })
+    .then((result) => {
+      res.status(result.status).json(result, privKey);
+    })
+    .catch((err) => {
+      res.status(200).json(err);
+    });
+
+  console.log(hash);
+
+  a;
+  res.status(200).json({ msg: "success" });
 });
 
 router.get("/check-register", (req, res) => {

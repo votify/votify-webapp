82df4902cad2854269591972af658f78f115f983
Bearnies <longvikhang@gmail.com>
2020-06-18 08:36:31 +0700

    POST & GET from vote, votelist

--- routes/index.js
+++ routes/index.js
@@ -23,16 +23,55 @@ router.post("/check-register", (req, res) => {
 
 router.get("/votelist", (req, res) => {
   res.render("votelist", { layout: "layout" });
+
+  const year = req.params['year'];
+  const nameOfElection = req.params['name'];
 });
 
 router.get("/votelist/:id", (req, res) => {
   const id = req.params['id'];
   res.render("vote", { layout: "layout" });
+
+  const year = req.params['year'];
+  const nameOfElection = req.params['name'];
+  const nominee = req.params['nominee'];
 });
 
 router.post("/votelist/:id", (req, res) => {
   const id = req.params['id'];
   res.render("vote", { layout: "layout" });
+
+  const data = { ...req.body };
+
+  const { year, name, nominee, privKey } = data;
+
+  const convertedPrivKey = Buffer.from(privKey, 'utf8');
+
+  const pubKey = secp256k1.publicKeyCreate(convertedPrivKey);
+
+  const signObj = secp256k1.ecdsaSign(msg, convertedPrivKey)
+
+  const _data = {
+    data: { year: year, name: name, nominee: nominee },
+    signature: signObj,
+    address: pubKey,
+  };
+
+  const url = "https://blockchain-node-01.herokuapp.com/";
+  await axios
+    .post(url, { _data })
+    .then((result) => {
+      res.status(result.status).json(result, convertedPrivKey);
+    })
+    .catch((err) => {
+      res.status(200).json(err);
+    });
+
+  console.log(hash);
+
+  a;
+  res.status(200).json({ msg: "success" });
+  
 });
 
 module.exports = router;
